70179b141847a7e29dfa0444ea175e8e3c07f711
Shayne Trosdahl <shayne@trozdol.com>
2020-05-13 14:33:13 -0500

    More initial files

---
+++ app.js
@@ -0,0 +1,20 @@
+/*
+ * This file launches the application by asking Ext JS to create
+ * and launch() the Application class.
+ */
+Ext.application({
+    extend: 'Demo.Application',
+
+    name: 'Demo',
+
+    requires: [
+        'Demo.util.Renderer',
+        'Demo.view.main.Main'
+    ],
+    
+    models: [ 'Personnel', 'User', 'Company', 'Task' ],
+
+    stores: [ 'Personnel', 'Users', 'Companies', 'Tasks' ],
+
+    mainView: 'Demo.view.main.Main'
+});
